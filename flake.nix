a5dce0e51e8342ffec5e7cf01a51cce3792f3083
James Benze <james@benze.me>
2025-11-01 16:59:38 -0400

    Add nix setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development environment for Dayton Swing Smackdown Website";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [ pkgs.deno ];
+        };
+      }
+    );
+}
